a347edebb93b93817ed16cdcf0bdbf671172a623
tim <timoth.david@gmail.com>
2022-09-26 15:40:45 +0200

    add package-lock.json

---
+++ src/interface/RentResponse.ts
@@ -0,0 +1,10 @@
+export interface RentResponse {
+  id: string;
+  deposit: number;
+  address: string;
+  email: string;
+  description_in: string;
+  date_in: string;
+  description_out: string;
+  date_out: string;
+}
